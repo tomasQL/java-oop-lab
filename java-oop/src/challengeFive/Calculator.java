c19593c9d0571d196f95d1d4268046232493a745
tomasQL_MJ <tomasgaticagomez@gmail.com>
2023-03-16 19:46:54 -0300

    Challenge 5 added

--- java-oop/src/challengeFive/Calculator.java
+++ java-oop/src/challengeFive/Calculator.java
@@ -1,4 +1,33 @@
 package challengeFive;
 
 public class Calculator {
+
+    private double num1, num2;
+    public Calculator(double num1, double num2){
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    double add(){
+        return this.num1 + this.num2;
+    }
+    double substract(){
+        return this.num2 - this.num1;
+    }
+    double multiply(){
+        return this.num1 * this.num2;
+    }
+    double divide(){
+        return this.num2 / this.num1;
+    }
+}
+
+class Demo{
+    public static void main(String[] args) {
+        Calculator obj = new Calculator(10, 94);
+        System.out.println("El calculo de las operaciones es: ");
+        System.out.println(obj.add());
+        System.out.println(obj.substract());
+        System.out.println(obj.multiply());
+        System.out.println(obj.divide());
+    }
 }
